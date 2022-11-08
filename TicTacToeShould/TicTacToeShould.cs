8fe4b913c399604d339bb3b641608b89dbc7b787
Aaron García <aargarcia@aidacanarias.com>
2022-11-08 11:56:52 +0100

    green : have_initial_board_without_marks

--- TicTacToeShould/TicTacToeShould.cs
+++ TicTacToeShould/TicTacToeShould.cs
@@ -23,7 +23,7 @@ namespace TicTacToeShould {
         public char [,] Board { get;}
 
         public TicTacToeGame() {
-            Board = new char[3,3];
+            Board = new char[,] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
         }
     }
 }
