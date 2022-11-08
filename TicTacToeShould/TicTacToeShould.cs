f630598adffd397ef2eca483e8d431864fdc5783
Aaron García <aargarcia@aidacanarias.com>
2022-11-08 11:56:21 +0100

    red : have_initial_board_without_marks

--- TicTacToeShould/TicTacToeShould.cs
+++ TicTacToeShould/TicTacToeShould.cs
@@ -1,7 +1,29 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 
 namespace TicTacToeShould {
     public class TicTacToeShould{
+        [Test]
+        public void have_initial_board_without_marks() {
+            var game = new TicTacToeGame();
 
+            game.Board[0, 0].Should().Be('1');
+            game.Board[0, 1].Should().Be('2');
+            game.Board[0, 2].Should().Be('3');
+            game.Board[1, 0].Should().Be('4');
+            game.Board[1, 1].Should().Be('5');
+            game.Board[1, 2].Should().Be('6');
+            game.Board[2, 0].Should().Be('7');
+            game.Board[2, 1].Should().Be('8');
+            game.Board[2, 2].Should().Be('9');
+        }
+    }
+
+    public class TicTacToeGame {
+        public char [,] Board { get;}
+
+        public TicTacToeGame() {
+            Board = new char[3,3];
+        }
     }
 }
